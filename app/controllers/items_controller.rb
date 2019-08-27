78a0085a0fc16cb7b4916a697f715e0ba212ec81
Raquel Minasi <raquelminasi@gmail.com>
2019-08-27 13:06:35 +0100

    items controller created, index method done, also view

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,33 @@
+class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  #   def new
+  #     @item = Item.new(item_params)
+  #     @item.owner = current_user
+  #     if @item.save
+  #       redirect_to item_path(@item)
+  #     else
+  #       render :new
+  #     end
+  #   end
+
+  #   def update
+  #   end
+
+  #   def destroy
+  #   end
+end
+
+private
+
+def item_params
+  param.require(:item).permit(:name, :description, :location, :question1, :question2, :question3)
+end
