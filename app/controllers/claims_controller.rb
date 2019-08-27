6ff13b8c7545e81830a0a46ff0021b315c2b6bb1
Susan Renaud <serenaud89@gmail.com>
2019-08-27 09:32:44 -0500

    created new and create controller actions and view for claims controller

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,4 +1,23 @@
 class ClaimsController < ApplicationController
-  def
+  before_action :authenticate_user!, except: [:show, :index]
+  def new
+    @claim = Claim.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @claim = Claim.new(params[claim_params])
+    @claim.user = @item
+    if @claim.save
+      redirect_to new_claim_path
+    else
+      redirect_to claims_path
+    end
+  end
+
+  private
+
+  def claim_params
+    params.require(:claim).permit(:answer1, :answer2, :answer3)
   end
 end
