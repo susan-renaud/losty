856e5823cf9222902c285c61b8be0dfd7b4eb18c
Susan Renaud <serenaud89@gmail.com>
2019-08-27 11:40:08 -0500

    wip claims

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:show, :index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @items = Item.all
