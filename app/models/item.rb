9cd9aebfb3ce6d34effbb234c490d351d4432d3d
Susan Renaud <serenaud89@gmail.com>
2019-08-29 07:18:12 -0500

    made search bar searching by naem of item and location, added to navbar

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,9 @@ class Item < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_location,
+                  against: [:name, :location],
+                  using: { tsearch: { prefix: true } }
 end
