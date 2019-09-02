782da159ef0740f5b5328078ecdc674edf5f20b5
Susan Renaud <serenaud89@gmail.com>
2019-09-02 07:09:18 -0500

    completed styling of index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,25 +20,30 @@ Item.create(user: susan, name: 'Jacket', remote_photo_url: 'https://images.unspl
    location: 'Hyde Park', question1: 'What size is this jacket?', question2: 'What brand is the jacket?',
   question3: 'Is there anything in the pockets?')
 
+Item.create(user: steph, name: 'Backpack', remote_photo_url: 'https://images.unsplash.com/photo-1541267732407-8f72c182cf11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80',
+   description: 'This backpack was found in a restaurant in Canary Wharf.',
+   location: 'Canary Wharf', question1: 'What brand is the backpack?', question2: 'What is inside the backpack?',
+  question3: 'What restaurant was the backpack found in?')
+
 Item.create(user: raquel, name: 'Wallet', remote_photo_url: 'https://images.unsplash.com/photo-1512414947060-048d53abb081?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
    description: 'I found this brown wallet today at the overground Hoxton stop.',
    location: 'Hoxton', question1: 'Which platform was it on?', question2: 'What brand is the wallet?',
   question3: 'Anything that stands out inside of the wallet?')
 
-Item.create(user: steph, name: 'Backpack', remote_photo_url: 'https://images.unsplash.com/photo-1541267732407-8f72c182cf11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80',
-   description: 'This backpack was found in a restaurant in Canary Wharf.',
-   location: 'Canary Wharf', question1: 'What brand is the backpack?', question2: 'What is inside the backpack?',
-  question3: 'What restaurant was the backpack found in?')
+Item.create(user: raquel, name: 'Gloves', remote_photo_url: 'https://images.unsplash.com/photo-1515951806792-63ac4250f2f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+   description: 'I found these winter gloves at the library.',
+   location: 'Kings Cross, London', question1: 'Are the adult or childrens gloves?', question2: 'What color is the lining?',
+  question3: 'There is a stain on them, what color is it?')
 
 Item.create(user: susan, name: 'Fancy fountain pen', remote_photo_url: 'https://images.unsplash.com/photo-1457298483369-0a95d2b17fcd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
    description: 'This is a really nice fountain pen and I am sure the owner is missing it!',
    location: 'SE9 6BT', question1: 'What color ink does it have?', question2: 'What color is the end of the pen?',
   question3: 'What name is etched on the cap of the fountain pen?')
 
-Item.create(user: raquel, name: 'Gloves', remote_photo_url: 'https://images.unsplash.com/photo-1515951806792-63ac4250f2f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
-   description: 'I found these winter gloves at the library.',
-   location: 'Kings Cross, London', question1: 'Are the adult or childrens gloves?', question2: 'What color is the lining?',
-  question3: 'There is a stain on them, what color is it?')
+Item.create(user: steph, name: 'Cap', remote_photo_url: 'https://images.unsplash.com/photo-1521369909029-2afed882baee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+   description: 'I found this cap was on a bench in Victoria Park.',
+   location: 'Victoria Park, London', question1: 'What brand is the cap?', question2: 'What color is the inside of the cap?',
+  question3: 'What is written on the inside of the cap?')
 
 
 
